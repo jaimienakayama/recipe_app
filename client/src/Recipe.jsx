2c26cea25d9cd353c30542224290bb6738aba5dd
jaimienakayama <jaimie.nakayama@icloud.com>
2022-01-05 18:13:32 -0500

    added dynamic rendering of font size depending on how many characters

--- client/src/Recipe.jsx
+++ client/src/Recipe.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 const Recipe = ({ name, url, onClick }) => {
+  const fontSize = name.length > 24 ? "medium-font" : "large-font";
+
   return (
     <div className="recipe-container" onClick={onClick}>
       <div className="thumbnail-container">
         <img src={url} alt={name} className="thumbnail" />
       </div>
-      <div className="recipe-title">{name}</div>
+      <div className={`recipe-title ${fontSize}`}>{name}</div>
     </div>
   );
 };
