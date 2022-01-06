017f787c86eece868105e3fb9b235ba2c24604f5
jaimienakayama <jaimie.nakayama@icloud.com>
2022-01-05 19:19:55 -0500

    added logic to only show 20 cards upon initial load and show more button to add more cards. also added back to top button

--- client/src/RecipeList.jsx
+++ client/src/RecipeList.jsx
@@ -1,28 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Recipe from "./Recipe.jsx";
+import Button from "./Button.jsx";
 
 const RecipeList = ({ recipes, setView, setRecipe }) => {
+  const [showing, setShowing] = useState([]);
+  const [max, setMax] = useState(16);
+
+  useEffect(() => {
+    if (recipes.length > 16) {
+      const segment = recipes.slice(0, max);
+      setShowing(segment);
+    } else {
+      setShowing(recipes);
+    }
+  }, [recipes, max]);
+
   const recipeOnClick = (recipe) => {
     setView("recipe");
     setRecipe(recipe);
     window.scrollTo(0, 0);
   };
 
+  const showMoreOnClick = () => {
+    setMax((prev) => prev + 8);
+  };
+
+  const backToTopOnClick = () => {
+    window.scrollTo(0, 0);
+    setMax(16);
+  };
+
+  console.log(max);
   return (
-    <div className="recipe-list">
-      {recipes &&
-        recipes.map((r) => {
-          const { name, url, components } = r;
-          return (
-            <Recipe
-              name={name}
-              url={url}
-              components={components}
-              onClick={() => recipeOnClick(r)}
-            />
-          );
-        })}
-    </div>
+    <>
+      <div className="recipe-list">
+        {showing &&
+          showing.map((r) => {
+            const { name, url, components } = r;
+            return (
+              <Recipe
+                name={name}
+                url={url}
+                components={components}
+                onClick={() => recipeOnClick(r)}
+              />
+            );
+          })}
+      </div>
+      <div className="footer">
+        {showing.length !== recipes.length ? (
+          <Button
+            btnClass="btn"
+            text="Show More"
+            onClick={() => showMoreOnClick()}
+          />
+        ) : (
+          <Button
+            btnClass="btn"
+            text="Back To Top"
+            onClick={() => backToTopOnClick()}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
